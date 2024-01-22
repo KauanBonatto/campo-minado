39e6e4e4f6b147977049760442701ac00bc7637e
Gabriel Mendes <szrfft2dhf@privaterelay.appleid.com>
2024-01-22 20:30:03 -0300

    feat: modal config and level outros

--- src/app/modal.tsx
+++ src/app/modal.tsx
@@ -4,7 +4,6 @@ import { faClose } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { ReactNode } from "react";
 
-
 interface Props {
   open: boolean;
   title: string;
@@ -12,16 +11,26 @@ interface Props {
   handleClose?: () => void;
 }
 
-const Modal: React.FC<Props> = ({ open, title, renderContent, handleClose }) => {
-
+const Modal: React.FC<Props> = ({
+  open,
+  title,
+  renderContent,
+  handleClose,
+}) => {
   if (!open) return null;
 
   return (
-    <main className="fixed w-1/2 h-max bg-white rounded-md">
+    <main className="fixed w-1/2 h-max bg-slate-700 rounded-md">
       <div className="p-8 w-full h-full">
-        <h1 className="text-black text-center font-bold mb-8">
-          {title} 
-          {handleClose && <FontAwesomeIcon onClick={handleClose} className="absolute right-5 text-xl text-red-600 hover:text-red-700 cursor-pointer" icon={faClose} />}
+        <h1 className="text-white text-center font-bold mb-8">
+          {title}
+          {handleClose && (
+            <FontAwesomeIcon
+              onClick={handleClose}
+              className="absolute right-5 text-xl text-red-600 hover:text-red-700 cursor-pointer"
+              icon={faClose}
+            />
+          )}
         </h1>
 
         {renderContent}
--- src/app/modalConfigs.tsx
+++ src/app/modalConfigs.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { ChangeEvent, useState } from "react";
 import Modal from "./modal";
 
 import type { GameConfigProps } from "./page";
@@ -10,47 +11,174 @@ interface Props {
   setGameConfig: React.Dispatch<React.SetStateAction<GameConfigProps>>;
 }
 
-const ModalConfig: React.FC<Props> = ({ modalConfigOpen, setModalConfigOpen, setGameConfig }) => {
+interface ConfigOutros {
+  qtdRows: number;
+  qtdColumns: number;
+  qtdBombs: number;
+}
+
+const ModalConfig: React.FC<Props> = ({
+  modalConfigOpen,
+  setModalConfigOpen,
+  setGameConfig,
+}) => {
+  const [isPersonalized, setIsPersonalized] = useState<boolean>(false);
+  const [configOutros, setConfigOutros] = useState<ConfigOutros>({
+    qtdRows: 5,
+    qtdColumns: 5,
+    qtdBombs: 3,
+  });
+
+  const levels = [
+    {
+      name: "NÍVEL 1",
+      levelSelected: "1",
+      qtdRows: 5,
+      qtdColumns: 5,
+      qtdBombs: 3,
+    },
+    {
+      name: "NÍVEL 2",
+      levelSelected: "2",
+      qtdRows: 8,
+      qtdColumns: 8,
+      qtdBombs: 6,
+    },
+    {
+      name: "NÍVEL 3",
+      levelSelected: "3",
+      qtdRows: 10,
+      qtdColumns: 10,
+      qtdBombs: 15,
+    },
+    {
+      name: "NÍVEL 4",
+      levelSelected: "4",
+      qtdRows: 12,
+      qtdColumns: 12,
+      qtdBombs: 28,
+    },
+    {
+      name: "NÍVEL 5",
+      levelSelected: "5",
+      qtdRows: 15,
+      qtdColumns: 15,
+      qtdBombs: 32,
+    },
+    {
+      name: "NÍVEL 6",
+      levelSelected: "6",
+      qtdRows: 18,
+      qtdColumns: 18,
+      qtdBombs: 58,
+    },
+    {
+      name: "OUTRO",
+    },
+  ];
   const handleLevelSelect = (value: GameConfigProps) => {
     setGameConfig(value);
     setModalConfigOpen(false);
   };
 
+  const handleLevelOutros = () => {
+    handleLevelSelect({
+      levelSelected: "OUTROS",
+      qtdRows: configOutros.qtdRows,
+      qtdColumns: configOutros.qtdColumns,
+      qtdBombs: configOutros.qtdBombs,
+    });
+    setConfigOutros({ qtdRows: 5, qtdColumns: 5, qtdBombs: 3 });
+    setIsPersonalized(false);
+  };
+
+  const handleChangeOutros = (event: ChangeEvent<HTMLInputElement>) => {
+    setConfigOutros({
+      ...configOutros,
+      [event.target.name]: event.target.value,
+    });
+  };
+
   return (
-    <Modal open={modalConfigOpen} title="CONFIGURAÇÕES" renderContent={
-      <div className="flex w-full h-full items-center m-auto">
-        <div className="flex w-full h-min flex-col gap-8">
-          <div className="flex m-auto gap-4 w-full max-w-xl">
-            <div className="w-1/4 flex justify-center cursor-pointer">
-              <p onClick={() => handleLevelSelect({ levelSelected: '1', qtdRows: 5, qtdColumns: 5, qtdBombs: 3 })} className="flex-1 p-3 bg-black text-white border border-black text-center font-bold rounded-md transition-all hover:bg-white hover:text-black">NÍVEL 1</p>
+    <Modal
+      open={modalConfigOpen}
+      title="CONFIGURAÇÕES"
+      renderContent={
+        isPersonalized ? (
+          <div className="flex w-full h-full items-end m-aut flex-wrap justify-center gap-4">
+            <div>
+              <p className="text-white mb-1">Número de Colunas</p>
+              <input
+                name="qtdColumns"
+                className="bg-slate-700 text-white border rounded-xl p-3"
+                type="number"
+                onChange={(e) => handleChangeOutros(e)}
+                value={configOutros.qtdColumns}
+              />
             </div>
-            <div className="w-1/4 flex justify-center cursor-pointer">
-              <p onClick={() => handleLevelSelect({ levelSelected: '2', qtdRows: 8, qtdColumns: 8, qtdBombs: 6 })} className="flex-1 p-3 bg-black text-white border border-black text-center font-bold rounded-md transition-all hover:bg-white hover:text-black">NÍVEL 2</p>
+            <div>
+              <p className="text-white mb-1">Número de Linhas</p>
+              <input
+                name="qtdRows"
+                className="bg-slate-700 text-white border rounded-xl p-3"
+                type="number"
+                onChange={(e) => handleChangeOutros(e)}
+                value={configOutros.qtdRows}
+              />
             </div>
-            <div className="w-1/4 flex justify-center cursor-pointer">
-              <p onClick={() => handleLevelSelect({ levelSelected: '3', qtdRows: 10, qtdColumns: 10, qtdBombs: 15 })} className="flex-1 p-3 bg-black text-white border border-black text-center font-bold rounded-md transition-all hover:bg-white hover:text-black">NÍVEL 3</p>
+            <div>
+              <p className="text-white mb-1">Número de Bombas</p>
+              <input
+                name="qtdBombs"
+                className="bg-slate-700 text-white border rounded-xl p-3"
+                type="number"
+                onChange={(e) => handleChangeOutros(e)}
+                value={configOutros.qtdBombs}
+              />
             </div>
-            <div className="w-1/4 flex justify-center cursor-pointer">
-              <p onClick={() => handleLevelSelect({ levelSelected: '4', qtdRows: 12, qtdColumns: 12, qtdBombs: 28 })} className="flex-1 p-3 bg-black text-white border border-black text-center font-bold rounded-md transition-all hover:bg-white hover:text-black">NÍVEL 4</p>
+            <button
+              className="py-4 px-10 rounded-2xl bg-slate-100 text-slate-900 text-center font-bold transition-all hover:bg-slate-300"
+              onClick={() => handleLevelOutros()}
+            >
+              JOGAR
+            </button>
+          </div>
+        ) : (
+          <div className="flex w-full h-full items-center m-aut">
+            <div className="flex justify-center w-full h-min flex-wrap gap-4">
+              {levels.map((level) => {
+                if (level.levelSelected) {
+                  return (
+                    <button
+                      className="py-4 px-10 rounded-2xl bg-slate-100 text-slate-900 text-center font-bold transition-all hover:bg-slate-300"
+                      onClick={() =>
+                        handleLevelSelect({
+                          levelSelected: level.levelSelected,
+                          qtdRows: level.qtdRows,
+                          qtdColumns: level.qtdColumns,
+                          qtdBombs: level.qtdBombs,
+                        })
+                      }
+                    >
+                      {level.name}
+                    </button>
+                  );
+                } else {
+                  return (
+                    <button
+                      className="py-4 px-10 rounded-2xl bg-slate-100 text-slate-900 text-center font-bold transition-all hover:bg-slate-300"
+                      onClick={() => setIsPersonalized(true)}
+                    >
+                      {level.name}
+                    </button>
+                  );
+                }
+              })}
             </div>
           </div>
-          <div className="flex m-auto gap-4 w-full max-w-xl">
-            <div className="w-1/4 flex justify-center cursor-pointer">
-              <p onClick={() => handleLevelSelect({ levelSelected: '5', qtdRows: 15, qtdColumns: 15, qtdBombs: 32 })} className="flex-1 p-3 bg-black text-white border border-black text-center font-bold rounded-md transition-all hover:bg-white hover:text-black">NÍVEL 5</p>
-            </div>
-            <div className="w-1/4 flex justify-center cursor-pointer">
-              <p onClick={() => handleLevelSelect({ levelSelected: '6', qtdRows: 18, qtdColumns: 18, qtdBombs: 58 })} className="flex-1 p-3 bg-black text-white border border-black text-center font-bold rounded-md transition-all hover:bg-white hover:text-black">NÍVEL 6</p>
-            </div>
-            <div className="w-1/4 flex justify-center cursor-pointer">
-              <p onClick={() => alert('EM DESENVOLVIMENTO...')} className="flex-1 p-3 bg-black text-white border border-black text-center font-bold rounded-md transition-all hover:bg-white hover:text-black">INFINITO</p>
-            </div>
-            <div className="w-1/4 flex justify-center cursor-pointer">
-              <p onClick={() => alert('EM DESENVOLVIMENTO...')} className="flex-1 p-3 bg-black text-white border border-black text-center font-bold rounded-md transition-all hover:bg-white hover:text-black">OUTRO</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    } />
+        )
+      }
+    />
   );
 };
 
